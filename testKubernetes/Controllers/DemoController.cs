876f680c8f88c71c535eed5c657f96c5469771c8
Gwladys <gwladyspontais@yahoo.com>
2023-06-29 14:02:49 +0200

    test

---
+++ testKubernetes/Controllers/DemoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace testKubernetes.Controllers
+{
+    [ApiController]
+    [Route("demo")]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet]
+        public String Get()
+        {
+            return "Welcome to the Docker World!";
+        }
+    }
+}
